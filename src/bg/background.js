50d052c6813b5a6d3123e731887469fbbdcb3a5f
Matt Pace <pace4@purdue.edu>
2016-07-14 01:54:00 -0400

    Manually execute script

--- src/bg/background.js
+++ src/bg/background.js
@@ -8,3 +8,9 @@ chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
       break
   }
 })
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.status === 'complete' && tab.url && tab.url.match('https:\/\/github\.com\/[a-zA-z1-9]+\/[a-zA-z0-9]+\/pull\/[0-9]+')) {
+    chrome.tabs.executeScript(tab.id, { file: 'src/inject/inject.js' })
+  }
+})
