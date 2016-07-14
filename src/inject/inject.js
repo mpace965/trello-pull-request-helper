deba8dca0193c1d006bd00af0a22ed9fa3adea0d
Matt Pace <pace4@purdue.edu>
2016-07-14 01:16:14 -0400

    Extract trello card url. Only modify pull request if it is found.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,17 +3,51 @@ chrome.extension.sendMessage({ action: 'add-classes' }, response => {
 		if (document.readyState === "complete") {
 			clearInterval(readyStateCheckInterval)
 
-			const container = document.getElementsByClassName('container new-discussion-timeline experiment-repo-nav')[0]
-			const issuesListing = container.getElementsByClassName('issues-listing')[0]
-			const pullRequest = issuesListing.getElementsByClassName('clearfix js-issues-results')[0]
-			const trelloDiv = issuesListing.appendChild(document.createElement('div'))
+			const threadId = document.querySelector('[name="thread_id"]').value
+			const issue = document.getElementById(`issue-${threadId}`)
+			const commentBody = issue.getElementsByClassName('comment-body markdown-body markdown-format js-comment-body')[0]
 
-			container.classList.add('inject-container')
-			issuesListing.classList.add('inject-issues-listing')
-			pullRequest.classList.add('inject-pull-request')
-			trelloDiv.classList.add('inject-trello-div')
+			const trelloCardURL = this.trelloCardURL(commentBody)
+
+			if (trelloCardURL) {
+				const container = document.getElementsByClassName('container new-discussion-timeline experiment-repo-nav')[0]
+				const issuesListing = container.getElementsByClassName('issues-listing')[0]
+				const pullRequest = issuesListing.getElementsByClassName('clearfix js-issues-results')[0]
+				const trelloDiv = issuesListing.appendChild(document.createElement('div'))
+
+				container.classList.add('inject-container')
+				issuesListing.classList.add('inject-issues-listing')
+				pullRequest.classList.add('inject-pull-request')
+				trelloDiv.classList.add('inject-trello-div')
+
+				chrome.extension.sendMessage({ action: 'inject-stylesheet' })
+			}
 		}
 	}, 10)
 })
 
-chrome.extension.sendMessage({ action: 'inject-stylesheet' })
+function trelloCardURL(commentBody) {
+	const trelloCardElement = this.trelloCardElement(commentBody)
+
+	if (trelloCardElement) {
+		return trelloCardElement.innerHTML.match('https:\/\/trello.com\/c\/[a-zA-z1-9]+\/[a-zA-z1-9-]+')[0]
+	}
+
+	return null
+}
+
+function trelloCardElement(commentBody) {
+	for (let i = 0; i < commentBody.childNodes.length; i++) {
+		element = commentBody.childNodes[i]
+
+		if (element.nodeName != 'P') {
+			continue
+		}
+
+		if (element.innerHTML.includes('trello.com/c/')) {
+			return element
+		}
+	}
+
+	return null
+}
